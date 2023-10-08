fe9f880c96f930cf3bd518183f23fcb55fb302a2
megagig <megagigsoftwaresolutions@gmail.com>
2023-10-08 22:21:17 +0100

    Create a Function to show the popular tv shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+//display 20 most  popular movies on home page
 const displayPopularMovies = async () => {
   const { results } = await fetchAPIData('movie/popular');
   results.forEach((movie) => {
@@ -30,6 +31,38 @@ const displayPopularMovies = async () => {
     </p>
   </div>`;
     document.getElementById('popular-movies').appendChild(div);
+  });
+};
+
+//display 20 most  popular TV shows on home page
+const displayPopularTvShows = async () => {
+  const { results } = await fetchAPIData('movie/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+  <a href="tv-details.html?id=${show.id}">
+    ${
+      show.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+        : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+  />`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${show.name}</h5>
+    <p class="card-text">
+      <small class="text-muted">Air Date: ${show.first_air_date}</small>
+    </p>
+  </div>`;
+    document.getElementById('popular-shows').appendChild(div);
   });
 };
 //fetch data from TMDB API
@@ -74,7 +107,7 @@ const init = () => {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularTvShows();
       break;
     case '/movie-details.html':
       console.log('Movie details');
