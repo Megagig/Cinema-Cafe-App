5bbbc0f1dc93b84b255041c2752abefacde79787
megagig <megagigsoftwaresolutions@gmail.com>
2023-10-08 20:28:19 +0100

    Use ForEach method to loop through Array created by the Fetch API

--- js/script.js
+++ js/script.js
@@ -4,7 +4,32 @@ const global = {
 
 const displayPopularMovies = async () => {
   const { results } = await fetchAPIData('movie/popular');
-  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+  <a href="movie-details.html?id=${movie.id}">
+    ${
+      movie.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+        : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt="Movie Title"
+/>`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">
+      <small class="text-muted">${movie.release_date}</small>
+    </p>
+  </div>`;
+  });
 };
 //fetch data from TMDB API
 
