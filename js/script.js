6482eb6366402d1ea663727a3d0ee6901e235b6d
megagig <megagigsoftwaresolutions@gmail.com>
2023-10-09 08:26:53 +0100

    Add regular Expression to add comma to JS numbers

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ const displayPopularMovies = async () => {
 
 //display 20 most  popular TV shows on home page
 const displayPopularTvShows = async () => {
-  const { results } = await fetchAPIData('movie/popular');
+  const { results } = await fetchAPIData('tv/popular');
   results.forEach((show) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -62,8 +62,62 @@ const displayPopularTvShows = async () => {
       <small class="text-muted">Air Date: ${show.first_air_date}</small>
     </p>
   </div>`;
-    document.getElementById('popular-shows').appendChild(div);
+    document.querySelector('#popular-shows').appendChild(div);
   });
+};
+
+//Display Movie Details
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+  ${
+    movie.poster_path
+      ? `<img
+  src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+      : `<img 
+src="images/no-image.jpg"
+class="card-img-top"
+alt="${movie.title}"
+/>`
+  }
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)}/ 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+    ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span>$${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+    <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+    <li><span class="text-secondary">Status:</span> Released</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">Company 1, Company 2, Company 3</div>
+</div>`;
+  document.querySelector('#movie-details').appendChild(div);
 };
 //fetch data from TMDB API
 
@@ -99,6 +153,10 @@ const hightlightActiveLink = () => {
     }
   });
 };
+// Search for Js function to add commas to numbers in Stack Overflow
+const addCommasToNumber = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 //initialize Application
 const init = () => {
   switch (global.currentPage) {
@@ -110,7 +168,7 @@ const init = () => {
       displayPopularTvShows();
       break;
     case '/movie-details.html':
-      console.log('Movie details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV details');
